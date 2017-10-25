e0685020e31f898d317c846177c7fd47a88d06d8
Dave Senciall <dave.senciall@dfo-mpo.gc.ca>
2017-10-25 13:43:00 -0230

    history block working

--- BIO_reader.py
+++ BIO_reader.py
@@ -34,8 +34,9 @@ class ODF_reader:
         self.hdrdict["PARAMETER_HEADER"] = OrderedDict()
         self.hdrdict["PARAMETER_HEADER"]["COUNT"]=0
         self.hdrdict["RECORD_HEADER"] = OrderedDict()
+        self.hdrdict["DATA"] = OrderedDict()
 
-        self.DATA = OrderedDict()
+#        self.DATA = OrderedDict()
 
 
 
@@ -51,23 +52,100 @@ class ODF_reader:
 
             self.endrow = 0
             current_dict = ''
-
+            buf =""
+            line = ''
             # parse through the HEADER blccks
-            for line in fp:
-                # stop once you hit the end of the header block
+            while True:
+#                for line in fp:
+
+                try:
+                    line = fp.readline()
+                except:
+                    break
+
+                if "-- DATA --" in line:
+                    n = 0
+                    while True:
+                        try:
+                            line = fp.readline()
+                            line = line.rstrip()
+                        except:
+                            return
+
+                        if n == 40:
+                                    return
+
+                        n=n+1
+                        print line
+                        self.hdrdict["DATA"][n] = line.split()
+
+
+
+
+
+
+
+#                print line
                 line = line.rstrip()
                 line = line.rstrip(',')
-                if "-- DATA --" in line:
-                    self.endrow += 1
-                    break
+
                 if line in self.hdrdict:
                     current_dict = line
 #                    print "current dict = ",current_dict
                 else:
-#                    print "line = ",line
-                    sub_parms = line.split("=")
-#                    print sub_parms[0], sub_parms[1]
-                    self.hdrdict[current_dict][sub_parms[0]] = sub_parms[1]
+                    if current_dict == "HISTORY_HEADER":
+                        self.hdrdict["HISTORY_HEADER"][self.hdrdict["HISTORY_HEADER"]["COUNT"]] = OrderedDict()
+                        line = line.rstrip()
+
+                        self.hdrdict["HISTORY_HEADER"][self.hdrdict["HISTORY_HEADER"]["COUNT"]][0] = line
+                        n=1
+                        line = fp.readline()
+                        line = line.rstrip()
+                        while line[-1:] ==',':
+
+                            self.hdrdict["HISTORY_HEADER"][self.hdrdict["HISTORY_HEADER"]["COUNT"]][n] = line
+                            n = n +1
+                            try:
+                                line = fp.readline()
+                                line = line.rstrip()
+                            except:
+                                break
+
+                        self.hdrdict["HISTORY_HEADER"][self.hdrdict["HISTORY_HEADER"]["COUNT"]][n] = line
+                        self.hdrdict["HISTORY_HEADER"]["COUNT"] = self.hdrdict["HISTORY_HEADER"]["COUNT"] + 1
+
+                        current_dict = ''
+
+                    elif current_dict == "PARAMETER_HEADER":
+
+                        self.hdrdict["PARAMETER_HEADER"][self.hdrdict["PARAMETER_HEADER"]["COUNT"]] = OrderedDict()
+                        line = line.rstrip()
+                        sub_parms = line.split("=")
+
+                        self.hdrdict["PARAMETER_HEADER"][self.hdrdict["PARAMETER_HEADER"]["COUNT"]][sub_parms[0]] = sub_parms[1]
+
+                        line = fp.readline()
+                        line = line.rstrip()
+                        print "LINES=", line[-1:]
+                        while line[-1:] == ',':
+                            line = line.rstrip(',')
+                            sub_parms = line.split("=")
+                            self.hdrdict["PARAMETER_HEADER"][self.hdrdict["PARAMETER_HEADER"]["COUNT"]][sub_parms[0]] = sub_parms[1]
+
+                            try:
+                                line = fp.readline()
+                                line = line.rstrip()
+                            except:
+                                break
+
+                        line = line.rstrip(',')
+                        sub_parms = line.split("=")
+                        self.hdrdict["PARAMETER_HEADER"][self.hdrdict["PARAMETER_HEADER"]["COUNT"]][sub_parms[0]] = sub_parms[1]
+                        self.hdrdict["PARAMETER_HEADER"]["COUNT"] = self.hdrdict["PARAMETER_HEADER"]["COUNT"] + 1
+                        current_dict = ''
+                    else:
+                        sub_parms = line.split("=")
+                        self.hdrdict[current_dict][sub_parms[0]] = sub_parms[1]
 
 
     def print_text(self):
